bd30769ebd3ec74d015f2fa192598b6dff875742
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-02 22:08:48 +0530

    fixed --view of sysreport

--- commands/sysreport.sh
+++ commands/sysreport.sh
@@ -2,22 +2,23 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 log_DIR="$(cd $SCRIPT_DIR && cd "../core/logs" && pwd)"
 
-if [ -f $log_DIR/sysreport.log ];then 
+if [ -f $log_DIR/sysreport.log ];then
     touch $log_DIR/sysreport.log
     chmod +x $log_DIR/sysreport.log
 fi
 
 sysreport() {
-    if [ "$1" == "--view" ]; then
-        echo "Latest system report"
-        echo -e "System uptime :\n\t$(uptime)"
-        echo -e "Total RAM Useage:\n$(free -h)"
-        echo -e "Total Disk Useage:\n$(df -h)"
-        echo -e "3 Most CPU-hogging processes:\n$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4)"
-    fi  
+    echo "Latest system report"
+    echo -e "System uptime :\n\t$(uptime)"
+    echo -e "Total RAM Useage:\n$(free -h)"
+    echo -e "Total Disk Useage:\n$(df -h)"
+    echo -e "3 Most CPU-hogging processes:\n$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4)"
 }
-
-sysreport $1
+if [ "$1" == "--view" ]; then
+    echo -e "$(cat $log_DIR/sysreport.log | tail -n 25)"
+else 
+    sysreport $1
+fi 
 
 echo -e "$(date '+%Y-%m-%d %H:%M:%S')-############################################################\n$(sysreport $1)\n" >> "$log_DIR/sysreport.log"
 
