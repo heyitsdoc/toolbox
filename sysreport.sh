4dfc5b70e1417b52195369f7822883fd6dd6b2e1
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-02 21:11:56 +0530

    added two more functions to sysreport

--- sysreport.sh
+++ sysreport.sh
@@ -5,4 +5,6 @@ if [ "$1" == "--view" ]; then
     echo "Latest system report"
     echo -e "System uptime :\n\t$(uptime)"
     echo -e "Total RAM Useage:\n$(free -h)"
+    echo -e "Total Disk Useage:\n$(df -h)"
+    echo -e "3 Most CPU-hogging processes:\n$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4)"
 fi
