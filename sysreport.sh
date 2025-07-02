0cfe199eee54305916c04b3cff68cdef1721157b
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-02 21:23:15 +0530

    added logging for sysreport

--- sysreport.sh
+++ sysreport.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+log_DIR="$(cd $SCRIPT_DIR && cd "core/logs" && pwd)"
 
-
-if [ "$1" == "--view" ]; then
-    echo "Latest system report"
-    echo -e "System uptime :\n\t$(uptime)"
-    echo -e "Total RAM Useage:\n$(free -h)"
-    echo -e "Total Disk Useage:\n$(df -h)"
-    echo -e "3 Most CPU-hogging processes:\n$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4)"
+if [ -f $log_DIR/sysreport.log ];then 
+    touch $log_DIR/sysreport.log
+    chmod +x $log_DIR/sysreport.log
 fi
+
+sysreport() {
+    if [ "$1" == "--view" ]; then
+        echo "Latest system report"
+        echo -e "System uptime :\n\t$(uptime)"
+        echo -e "Total RAM Useage:\n$(free -h)"
+        echo -e "Total Disk Useage:\n$(df -h)"
+        echo -e "3 Most CPU-hogging processes:\n$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4)"
+    fi  
+}
+
+sysreport $1
+
+echo -e "$(date '+%Y-%m-%d %H:%M:%S')-############################################################\n$(sysreport $1)\n" >> "$log_DIR/sysreport.log"
+
+
+
